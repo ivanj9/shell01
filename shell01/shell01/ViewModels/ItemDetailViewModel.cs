b8c6ab0c94ab29fc0bde31f753baf2a062664dc7
ivanj9 <ivanj9@gmail.com>
2020-04-18 22:15:12 +0200

    Add project files.

---
+++ shell01/shell01/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using shell01.Models;
+
+namespace shell01.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ shell01/shell01/ViewModels/Page2ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace shell01.ViewModels
+{
+    class Page2ViewModel : INotifyPropertyChanged
+    {
+        public Page2ViewModel() 
+        {
+            OKCommand = new Command(() => { });
+
+        }
+
+        string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value)
+                    return;
+
+                name = value;
+
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Name Entered: {Name}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Command OKCommand { get; }
+    }
+
+}
